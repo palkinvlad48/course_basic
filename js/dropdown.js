77582019e903900d1101b633559f269c6e6151a0
vladimir <palkinw@mail.ru>
2021-02-22 16:48:47 +0300

    поправил скрипт для выпадашек

--- js/dropdown.js
+++ js/dropdown.js
@@ -1,8 +1,10 @@
 /* удаляет drop */
 function deleteDrop() {
   window.onclick = function (event) {
-    if (!event.target.matches('.dropbtn')) {
-
+    if (!event.target.matches('.dropbtn') || (dubl > 1)) {
+      if (dubl > 1) {
+        dubl = 0
+      }
       const dropdowns = document.getElementsByClassName("dropdown-content");
       let i = 0
       for (i = 0; i < dropdowns.length; i++) {
@@ -18,20 +20,20 @@ deleteDrop()
 /* */
 /* переключает между скрытием и отображением раскрывающегося содержимого */
 let old = null // создается клон объекта, если был клик 
-
+let dubl = 0
 function clickDrop() {
   deleteDrop()
   $('.dropbtn').on('click', function (e) {
     this.classList.toggle("show")
     //console.log('this-' + this.classList);
     const dropId = $(this).data('id')
-
+    dubl++
     if (old) {
-      old.classList.remove('show');
-    } 
+      old.classList.remove('show')
+    }
 
     let myDropdown = document.getElementById(dropId);
- 
+
     myDropdown.classList.add("show")
     old = myDropdown
 
